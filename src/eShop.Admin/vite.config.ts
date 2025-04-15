6459e3634bebeac152473afa22e7e3ab0c52c82e
Tran Xuan Thanh <thanhthcsab@gmail.com>
2025-04-15 11:19:16 +0700

    chore(config): enable HTTPS and configure launch settings

--- src/eShop.Admin/vite.config.ts
+++ src/eShop.Admin/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "node:path";
+import os from "node:os";
+import fs from "fs";
+
+const homedir = os.homedir();
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5002,
+    https: {
+      key: fs.readFileSync(path.join(homedir, ".ssl/localhost/localhost-key.pem")),
+      cert: fs.readFileSync(path.join(homedir, ".ssl/localhost/localhost.pem")),
+    },
+  },
+});
